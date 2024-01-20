fdba1b02b24f796413fee01aaa81ecf46b50f85c
Thomas Vy <vythomas97@gmail.com>
2024-01-19 23:35:57 -0700

    replaces apostrophes with their escaped uni code

--- src/app/projects/(individual-projects)/billiard-game/page.tsx
+++ src/app/projects/(individual-projects)/billiard-game/page.tsx
@@ -10,9 +10,9 @@ function BilliardGame() {
       <p>
         Pool game that you can play in single player or two player. Sink all
         your balls to win; if your cue ball sinks, you lose. When playing, press
-        '1' to bring you back to the menu, press and hold spacebar to charge
-        your shots. Press 'b' to reset your shot power. After winning or losing,
-        press any key to reset the game. <br />
+        &apos;1&apos; to bring you back to the menu, press and hold spacebar to
+        charge your shots. Press &apos;b&apos; to reset your shot power. After
+        winning or losing, press any key to reset the game. <br />
         Written in Processing 3.
       </p>
     </IndividualProjects>
--- src/app/projects/(individual-projects)/moshirlearning/page.tsx
+++ src/app/projects/(individual-projects)/moshirlearning/page.tsx
@@ -12,12 +12,12 @@ function MoshirLearning() {
         D2L). Professors can create courses, set the course active or inactive,
         enroll/unenroll students, upload assignments, download and grade
         submissions, send group emails to students. A student can view courses
-        they're enrolled in and that are active, view and download assignments,
-        view grades, upload assignment submissions, view other students enrolled
-        in the class, and send an email to the professor. In the video, the
-        program on the left is the professor and the program on the right is the
-        student. This project was completed for ENSF 409 in 2017 and won
-        outstanding project. <br />
+        they&apos;re enrolled in and that are active, view and download
+        assignments, view grades, upload assignment submissions, view other
+        students enrolled in the class, and send an email to the professor. In
+        the video, the program on the left is the professor and the program on
+        the right is the student. This project was completed for ENSF 409 in
+        2017 and won outstanding project. <br />
         Written in Java, MySQL, TCP sockets, MVC.
       </p>
     </IndividualProjects>
